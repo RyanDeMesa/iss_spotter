0e6c099ee63ab30716f593d8a074c12afe604b22
RyanDeMesa <ryandemesa09@gmail.com>
2022-11-25 03:02:25 +0000

    Created a function to find ip address

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// TEST
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
